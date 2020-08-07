746de8a1b08c20db2671061ee1eebc36d9d5c5c2
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-08-07 01:06:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,11 +3,11 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
  
  
-  def initialize(sender, receiver, amount)
+  def initialize(sender, receiver, amount = 50)
     @sender = sender
     @receiver = receiver
     @status = "pending"
-    @amount = 50
+    @amount = amount
   end  
   
   
